245558f2c9e40eb358f25f64cedbe2479c690b36
GinkaKuzmanov <ginkakuzmanova@gmail.com>
2020-09-15 23:02:50 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TaskContext from "./TaskContext";
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css";
+import Header from "./components/Header";
 
-export default App;
+const App = () => {
+    return (
+        <TaskContext>
+            <div className="container">
+                <div className="app-wrapper">
+                    <Header />
+                    <div className="main">
+                        <TaskForm />
+                        <TaskList />
+                    </div>
+                </div>
+            </div>
+        </TaskContext>
+    );
+};
+
+export default App
---
+++ src/TaskContext.js
@@ -0,0 +1,66 @@
+import React, {createContext, useEffect, useState} from 'react'
+import uuid from 'uuid'
+
+export const TaskContext = createContext()
+
+const TaskContextProvider = props => {
+    const initialState = JSON.parse(localStorage.getItem('tasks')) || []
+
+    const [tasks, setTasks] = useState(initialState)
+
+    useEffect(() => {
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    }, [tasks])
+
+    const [editItem, setEditItem] = useState(null)
+
+    // Add tasks
+    const addTask = title => {
+        setTasks([...tasks, {title, id: uuid()}])
+    }
+
+    // Remove tasks
+    const removeTask = id => {
+        setTasks(tasks.filter(task => task.id !== id))
+    }
+
+    // Clear tasks
+    const clearList = () => {
+        setTasks([])
+    }
+
+    // Find task
+    const findItem = id => {
+        const item = tasks.find(task => task.id === id)
+
+        setEditItem(item)
+    }
+
+    // Edit task
+    const editTask = (title, id) => {
+        const newTasks = tasks.map(task => (task.id === id ? {title, id} : task))
+
+        console.log(newTasks)
+
+        setTasks(newTasks)
+        setEditItem(null)
+    }
+
+    return (
+        <TaskContext.Provider
+            value={{
+                tasks,
+                addTask,
+                removeTask,
+                clearList,
+                findItem,
+                editTask,
+                editItem
+            }}
+        >
+            {props.children}
+        </TaskContext.Provider>
+    )
+}
+
+export default TaskContextProvider
